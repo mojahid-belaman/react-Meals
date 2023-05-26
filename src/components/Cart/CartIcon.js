93aa5ede86bb06fe6f66416d3829020adbd0f650
mojahid-belaman <mojahidbelaman@gmail.com>
2023-05-26 18:51:20 +0100

    fix some warning in named props

--- src/components/Cart/CartIcon.js
+++ src/components/Cart/CartIcon.js
@@ -9,11 +9,11 @@ const CartIcon = () => {
       viewBox="0 0 44.16 44.16"
       fill="#FFFFFF"
     >
-      <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+      <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
       <g
         id="SVGRepo_tracerCarrier"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       ></g>
       <g id="SVGRepo_iconCarrier">
         {" "}
