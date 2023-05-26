5a004b162af39bad3c2af23d0f947ef9f2975b8e
mojahid-belaman <mojahidbelaman@gmail.com>
2023-05-26 13:42:22 +0100

    add component Header

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,15 +1,16 @@
 import React, { Fragment } from "react";
 import meals from "../asset/meals.jpg"
+import HeaderCartButton from "./HeaderCartButton";
 
 const Header = () => {
   return (
     <Fragment>
-      <header className="w-full h-16 px-10 bg-primary text-white flex justify-between items-center">
+      <header className="w-full h-16 px-2 py-10 md:px-10 bg-primary text-white flex justify-between items-center">
         <h1 className="text-2xl font-bold">ReactMeals</h1>
-        <button>Cart</button>
+        <HeaderCartButton />
       </header>
-      <div className="h-96 overflow-hidden">
-        <img src={meals} alt="Meals Order" className="w-full h-full object-cover [transform:rotateZ(-5deg)_translateY(-4rem)_translateX(-1rem)_scale(1.1)]" />
+      <div className="h-100 overflow-hidden">
+        <img src={meals} alt="Meals Order" className="w-full h-full object-cover [transform:rotateZ(-3deg)_translateY(-5rem)_translateX(0)_scale(1.1)]" />
       </div>
     </Fragment>
   );
