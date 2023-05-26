34ee27c2cf9373956443944069aec29c4ba9b005
mojahid-belaman <mojahidbelaman@gmail.com>
2023-05-26 13:41:17 +0100

    add svg in component CartIcon

---
+++ src/components/Cart/CartIcon.js
@@ -0,0 +1,42 @@
+const CartIcon = () => {
+  return (
+    <svg
+      height="25px"
+      width="25px"
+      version="1.1"
+      id="Capa_1"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 44.16 44.16"
+      fill="#FFFFFF"
+    >
+      <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+      <g
+        id="SVGRepo_tracerCarrier"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      ></g>
+      <g id="SVGRepo_iconCarrier">
+        {" "}
+        <g>
+          {" "}
+          <path
+            style={{ fill: "#FFFFFF" }}
+            d="M44.16,6.6H11.592L10.66,1.179H2.408C1.076,1.179,0,2.257,0,3.588v2.408h6.602l4.248,24.709 c0.094,0.544,0.617,0.985,1.17,0.985h28.527c1.332,0,2.41-1.077,2.41-2.411v-2.406H15.078l-0.587-3.414h22.042 c2.66,0,5.172-2.128,5.611-4.75L44.16,6.6z"
+          ></path>{" "}
+          <circle
+            style={{ fill: "#FFFFFF" }}
+            cx="19.47"
+            cy="38.817"
+            r="4.165"
+          ></circle>{" "}
+          <path
+            style={{ fill: "#FFFFFF" }}
+            d="M29.762,38.816c0,2.299,1.863,4.164,4.162,4.164c2.301,0,4.168-1.865,4.168-4.164 c0-2.299-1.867-4.166-4.168-4.166C31.625,34.65,29.762,36.518,29.762,38.816z"
+          ></path>{" "}
+        </g>{" "}
+      </g>
+    </svg>
+  );
+};
+
+export default CartIcon;
