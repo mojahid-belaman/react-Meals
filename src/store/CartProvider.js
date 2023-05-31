7f4ef8e56147463cdb4d82b5601ecd6f27e34526
mojahid-belaman <mojahidbelaman@gmail.com>
2023-05-31 18:51:41 +0100

    create store cart

---
+++ src/store/CartProvider.js
@@ -0,0 +1,20 @@
+import React from "react";
+import CartContext from "./cart-context";
+
+function CartProvider(props) {
+  const addItemHandler = (item) => {};
+  const removeItemHandler = (id) => {};
+  const cartContext = {
+    item: [],
+    totalAmount: 0,
+    addItem: addItemHandler,
+    removeItem: removeItemHandler,
+  };
+  return (
+    <CartContext.Provider value={cartContext}>
+      {props.children}
+    </CartContext.Provider>
+  );
+}
+
+export default CartProvider;
