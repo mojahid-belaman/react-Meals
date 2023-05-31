3b2b84abce04a6d97eaf1fdffd5d60c6b879340b
mojahid-belaman <mojahidbelaman@gmail.com>
2023-05-31 16:14:36 +0100

    render Cart

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./index.css";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
 
 function App() {
   return (
     <div className="h-screen">
+      <Cart />
       <Header />
       <main>
         <Meals />
