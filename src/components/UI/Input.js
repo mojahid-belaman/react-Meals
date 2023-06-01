52906727d8193d028cca0704abc814f8536d26ea
mojahid-belaman <mojahidbelaman@gmail.com>
2023-06-01 15:03:08 +0100

    pass ref from my custom component to HTML-element by react forward

--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -1,12 +1,18 @@
 import React from "react";
 
-const Input = (props) => {
+const Input = React.forwardRef((props, ref) => {
   return (
     <div className="flex items-center space-x-4 py-2">
-      <label htmlFor={props.input.id} className="font-bold">{props.label}</label>
-      <input className="pl-2 rounded-lg border border-gray-300 outline-none" {...props.input} />
+      <label htmlFor={props.input.id} className="font-bold flex-1">
+        {props.label}
+      </label>
+      <input
+        ref={ref}
+        className="pl-2 max-w-[50px] rounded-lg border border-gray-300 outline-none"
+        {...props.input}
+      />
     </div>
   );
-};
+});
 
 export default Input;
