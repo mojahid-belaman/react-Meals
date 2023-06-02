2dacb864acb814dd59d312da655bf7be2c44e091
mojahid-belaman <mojahidbelaman@gmail.com>
2023-06-02 01:18:27 +0100

    map for every cart item and make condition if no items not display button order

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
 import Button from "../UI/Button";
 import Modal from "../UI/Modal";
+import CartContext from "../../store/cart-context";
+import CartItem from "./CartItem";
 
 const Cart = (props) => {
+  const cartCtx = useContext(CartContext);
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+  const hasItems = cartCtx.items.length > 0;
   const cartItems = (
-    <ul>
-      {[{ id: "c1", name: "Suchi", amount: 2, price: 12.99 }].map((item) => (
-        <li key={item.id}>{item.name}</li>
+    <ul className="mb-10 max-h-96 overflow-y-scroll scrollbar-thin scrollbar-thumb-primary">
+      {cartCtx.items.map((item) => (
+        <CartItem key={item.id} item={item}/>
       ))}
     </ul>
   );
@@ -15,7 +20,7 @@ const Cart = (props) => {
       {cartItems}
       <div className="flex justify-between text-2xl font-bold mb-5">
         <span>Total Amount</span>
-        <span>35.62</span>
+        <span>{totalAmount}</span>
       </div>
       <div className="space-x-4 text-right">
         <Button
@@ -26,9 +31,15 @@ const Cart = (props) => {
         >
           Close
         </Button>
-        <Button className={"px-8 py-1 text-white border border-primary rounded-full"}>
-          Order
-        </Button>
+        {hasItems && (
+          <Button
+            className={
+              "px-8 py-1 text-white border border-primary rounded-full"
+            }
+          >
+            Order
+          </Button>
+        )}
       </div>
     </Modal>
   );
