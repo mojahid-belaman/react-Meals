80f35f0aeb677076b0b5a55693aa17bee53e8bc3
mojahid-belaman <mojahidbelaman@gmail.com>
2023-06-05 18:27:25 +0100

    use ternary operator

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -25,7 +25,7 @@ const HeaderCartButton = (props) => {
 
   return (
     <button
-      className={`flex items-center space-x-2 md:space-x-4 bg-thirdly hover:bg-fourthly active:bg-fourthly text-white rounded-full px-5 md:px-10 py-3 ${animate && 'animate-bump'}`}
+      className={`flex items-center space-x-2 md:space-x-4 bg-thirdly hover:bg-fourthly active:bg-fourthly text-white rounded-full px-5 md:px-10 py-3 ${animate ? 'animate-bump' : ''}`}
       onClick={props.onClick}
     >
       <span>
