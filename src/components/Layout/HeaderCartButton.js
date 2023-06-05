ff47720ae2e655a4fee6d58095a0f1b6110c64ae
mojahid-belaman <mojahidbelaman@gmail.com>
2023-06-05 17:50:17 +0100

    add animation with useEffect

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,31 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import CartIcon from "../Cart/CartIcon";
 import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = (props) => {
+  const [animate, setAnimate] = useState(false);
   const cartCtx = useContext(CartContext);
-  const totalAmount = cartCtx.items.reduce((accum, curr) => {
+  const {items} = cartCtx
+  const totalAmount = items.reduce((accum, curr) => {
     return accum + curr.amount;
   }, 0);
+  useEffect(() => {
+    if (items.length === 0)
+      return;
+    setAnimate(true)
+
+    const timer = setTimeout(() => {
+      setAnimate(false);
+    }, 300)
+
+    return () => {
+      clearTimeout(timer);
+    }
+  }, [items])
+
   return (
     <button
-      className="flex items-center space-x-2 md:space-x-4 bg-thirdly hover:bg-fourthly active:bg-fourthly text-white rounded-full px-5 md:px-10 py-3"
+      className={`flex items-center space-x-2 md:space-x-4 bg-thirdly hover:bg-fourthly active:bg-fourthly text-white rounded-full px-5 md:px-10 py-3 ${animate && 'animate-bump'}`}
       onClick={props.onClick}
     >
       <span>
