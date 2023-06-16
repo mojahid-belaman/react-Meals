8a5e2660c5d16a916bc42133e8789274b8ed6ae2
mojahid-belaman <mojahidbelaman@gmail.com>
2023-06-16 15:35:54 +0100

    create function to fetch data from backend and delete dummy data

--- src/components/Meals/MealsAvailable.js
+++ src/components/Meals/MealsAvailable.js
@@ -1,17 +1,60 @@
-import React from "react";
 import Card from "../UI/Card";
 import MealsItem from "./MealsItem/MealsItem";
-import { DUMMY_MEALS } from "../../dummyData/meals";
+import { useEffect, useState } from "react";
 
 const MealsAvailable = () => {
-  const listMeals = DUMMY_MEALS.map((meal) => (
-    <MealsItem key={meal.id} meal={meal} />
-  ));
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      setError(null);
+      const res = await fetch(
+        "https://meal-http-fdda1-default-rtdb.firebaseio.com/meals.json"
+      );
+      if (!res.ok) {
+        throw new Error("Somthing Went Wrong!");
+      }
+      const data = await res.json();
+      const arrData = [];
+      for (const key in data) {
+        arrData.push({
+          id: key,
+          name: data[key].name,
+          description: data[key].description,
+          price: data[key].price,
+        });
+      }
+      setMeals(arrData);
+      setIsLoading(false);
+    };
+    fetchData().catch((error) => {
+      setError(error.message);
+      setIsLoading(false);
+    });
+  }, []);
+
+  let content = <p>No Found Meals!</p>;
+  if (meals.length > 0) {
+    content = (
+      <ul>
+        {meals.map((meal) => (
+          <MealsItem key={meal.id} meal={meal} />
+        ))}
+      </ul>
+    );
+  }
+  if (isLoading) {
+    content = <p>LOADING...</p>;
+  }
+  if (error) {
+    content = <p className="text-center text-red-600">{error}</p>;
+  }
   return (
     <section className="max-w-4xl w-[90%] mx-auto animate-meal">
-      <Card>
-        <ul>{listMeals}</ul>
-      </Card>
+      <Card>{content}</Card>
     </section>
   );
 };
