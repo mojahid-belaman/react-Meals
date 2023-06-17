34202a7a779a0ffcb812e527e7f1e6733a764e90
mojahid-belaman <mojahidbelaman@gmail.com>
2023-06-17 16:40:44 +0100

    use custom hook to post data

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -4,10 +4,13 @@ import Modal from "../UI/Modal";
 import CartContext from "../../store/cart-context";
 import CartItem from "./CartItem";
 import Checkout from "./Checkout";
+import useRequest from "../../hooks/use-Request";
 
 const Cart = (props) => {
   const [isCheckout, setIsCheckout] = useState(false);
+  const [isSubmit, setIsSubmit] = useState(false);
   const cartCtx = useContext(CartContext);
+  const { loading, error, fetchData } = useRequest();
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
   const cartItemAddHandler = (item) => {
@@ -35,6 +38,27 @@ const Cart = (props) => {
   const checkoutHandler = () => {
     setIsCheckout(true);
   };
+  const funcManupData = (data) => {
+    console.log(data);
+  };
+  const submitHandler = async (userInfo) => {
+    fetchData(
+      {
+        url: "https://meal-http-fdda1-default-rtdb.firebaseio.com/ordered.json",
+        method: "POST",
+        body: JSON.stringify({
+          user: userInfo,
+          orderedItems: cartCtx.items,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      },
+      funcManupData
+    );
+    cartCtx.clearItem();
+    setIsSubmit(true);
+  };
 
   const styleBtnCancel =
     "px-8 py-1 bg-white text-primary hover:bg-primary hover:text-white border border-primary rounded-full";
@@ -53,17 +77,37 @@ const Cart = (props) => {
       )}
     </div>
   );
-  return (
-    <Modal onClose={props.onClose}>
+
+  let content = (
+    <>
       {cartCtx.items.length > 0 && cartItems}
       <div className="flex justify-between text-2xl font-bold mb-5">
         <span>Total Amount</span>
         <span>{totalAmount}</span>
       </div>
-      {isCheckout && <Checkout onClose={props.onClose} />}
+      {isCheckout && (
+        <Checkout onConfirm={submitHandler} onClose={props.onClose} />
+      )}
       {!isCheckout && contentButton}
-    </Modal>
+    </>
   );
+  if (isSubmit) {
+    content = (
+      <>
+        <h1 className="text-primary mb-5">Data Save Successfully.</h1>
+        <Button className={styleBtnOrder + ` flex ml-auto`} onClick={props.onClose}>
+          Cancel
+        </Button>
+      </>
+    );
+  }
+  if (loading) {
+    content = <h1>Loading...</h1>;
+  }
+  if (error) {
+    content = <h1 className="text-red-600">{error}</h1>;
+  }
+  return <Modal onClose={props.onClose}>{content}</Modal>;
 };
 
 export default Cart;
