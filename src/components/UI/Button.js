a03709ad2d3aa09e7687b7838ed0b741864b7069
mojahid-belaman <mojahidbelaman@gmail.com>
2023-06-17 16:38:02 +0100

    add other property as disabled

--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -2,7 +2,12 @@ import React from "react";
 
 const Button = (props) => {
   return (
-    <button className={`bg-primary hover:bg-thirdly ${props.className}`} type={props.type || 'button'} onClick={props.onClick}>
+    <button
+      className={`bg-primary hover:bg-thirdly ${props.className}`}
+      type={props.type || "button"}
+      onClick={props.onClick}
+      disabled={props.disabled}
+    >
       {props.children}
     </button>
   );
